962c99520a0c4fdfd9ca9a4accfe386f5413fca5
Джалилов Евгений Викторович <zodimmortal@yandex.ru>
2025-09-16 17:06:12 +0300

    Добавил комментарий в Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { useClerk, useUser, UserButton } from "@clerk/clerk-react"; // Импо�
 //import {  } from "react"; // Закомментированный импорт. Вероятно, планировалось использовать что-то еще из React, но пока не реализовано.
 
 // Компонент SVG иконки книги. Используется для отображения в меню пользователя.
+{/* Картинка не работает ссылка xmlns="http://www.w3.org/2000/svg" надо потом заменить */}
 const BookIcon = ()=>(
     <svg className="w-4 h-4 text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm00a2 2 0 0 0 2 2h12M9 3v14m7 0v4"/>
